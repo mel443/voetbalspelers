60c3659e3b45f9817f8f77fd93af8e2df4e4e185
mel443 <155397970+mel443@users.noreply.github.com>
2024-01-05 00:50:16 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+class voetbalspelers:   
+def Naam van de voetbalspeler(self):        
+print("Steven Bergwijn.")   
+def Actuele voetbalclub(self):        
+print("Ajax."):
+ def Naam van de auteur(self):       
+print("Mel.")   
+ def Datum van invoer(self):       
+print("05-01-2024.")    
+ def Datum van laatste aanpassing(self):       
+print("04=01-2024.")    
